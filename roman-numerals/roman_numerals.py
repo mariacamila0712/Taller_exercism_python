7c1230fd3d9d538fe4d9e69b9701f3e9337c6232
MariaCamila <mariacmaza7@gmail.com>
2021-10-04 15:17:21 -0500

    Ejercicio 4 realizado

---
+++ roman-numerals/roman_numerals.py
@@ -0,0 +1,23 @@
+LETTERS = {
+    1000: "M",
+    900: "CM",
+    500: "D",
+    400: "CD",
+    100: "C",
+    90: "XC",
+    50: "L",
+    40: "XL",
+    10: "X",
+    9: "IX",
+    5: "V",
+    4: "IV",
+    1: "I",
+}
+
+
+def roman(number):
+    result = ""
+    for arabic, numeral in LETTERS.items():
+        times, number = divmod(number, arabic)
+        result += numeral * times
+    return result
